ee0af440e44c4697546300472700bde7d79d6acf
RubénEu <garcia.ruben@uma.es>
2021-03-19 10:45:03 +0100

    change version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name='simple-object-detection',
-    version='0.0.1.dev2',
+    version='0.0.1',
     description='Wrapper for the use of object detector with different models.',
     author='Rubén García Rojas',
     author_email='garcia.ruben@outlook.es',
