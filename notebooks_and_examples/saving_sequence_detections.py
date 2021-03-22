f4cb01d77327cb1df3f32b024874b0b2eed4145d
RubénEu <garcia.ruben@uma.es>
2021-03-22 16:25:09 +0100

    SOD-8. Fixed the output in the saving sequence detections example

--- notebooks_and_examples/saving_sequence_detections.py
+++ notebooks_and_examples/saving_sequence_detections.py
@@ -2,13 +2,13 @@ from simple_object_detection import DetectionModel
 from simple_object_detection.models import *
 from simple_object_detection.utils import *
 
-sequence = load_sequence('../../simple-object-tracking/sample_data/ball-8.mp4')
+_, _, _, sequence = load_sequence('../../simple-object-tracking/sample_data/ball-10.mp4')
 # Configurar la ruta de los modelos (local).
 DetectionModel.models_path = '../models_data/'
 
 # Configurar la red y guardar detecciones.
-net = FasterRCNNInceptionResnetV2(use_local=True)
-save_detections_in_sequence(net, sequence, '../../balls-8-detections.pkl')
+net = YOLOv3(use_local=True)
+save_detections_in_sequence(net, sequence, '../../balls-10-detections-yolov3.pkl')
 
 # Para todos los videos:
 # # Configurar la ruta de los modelos (local).
