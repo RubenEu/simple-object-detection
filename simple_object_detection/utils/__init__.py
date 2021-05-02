0d54ae597ae55499b90075aed6a518f70edf200d
RubenEu <garcia.ruben@outlook.es>
2021-05-03 00:25:12 +0200

    Arreglado imports

--- simple_object_detection/utils/__init__.py
+++ simple_object_detection/utils/__init__.py
@@ -2,7 +2,7 @@ from simple_object_detection.utils.image import load_image, draw_bounding_boxes
 from simple_object_detection.utils.video import StreamSequence, StreamSequenceWriter
 from simple_object_detection.utils.objects_detections import (generate_objects_detections,
                                                               save_objects_detections,
-                                                              load_object_detections,
+                                                              load_objects_detections,
                                                               filter_objects_by_classes,
                                                               filter_objects_by_min_score,
                                                               filter_objects_avoiding_duplicated)
